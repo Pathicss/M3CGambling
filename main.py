872127711bf8efd56ca53ac2dbe1551ab1532121
Pathicss <rigdonsbaylor@gmail.com>
2026-02-27 09:06:57 -0500

    Add files via upload
    main 2

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("whattup big daddy brandon oakes")
+import numpy
+import plotly_express as plt
+
+class individual:
+    def __init__(self, age, gender, salary, location, risk_tolerance):
+        self.age = age
+        self.gender = gender
+        self.salary = salary
+        self.location = location
+        self.risk_tolerance = risk_tolerance
+
